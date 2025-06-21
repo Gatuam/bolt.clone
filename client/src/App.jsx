a22b471bdb9fda3c6895883e9bfdf30b32e64e8c
sagar <sggtm42@gmail.com>
2025-06-21 03:22:50 -0400

    initialized client and server

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='w-full h-screen flex justify-center items-center bg-gradient-to-b from-[#000000] to-[#151515] text-white '>
+      hello
+    </div>
+  )
+}
+
+export default App
