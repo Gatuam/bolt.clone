e6cd6e57653b75361acfc3fcbe2d4dd561c18031
EK GAUTHAM <sggtm42@gmail.com>
2025-08-02 14:46:14 +0700

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,10 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://bolt-clone-675olvkfi-eks-projects-f96cd589.vercel.app",
+    origin: "https://bolt-clone-7bla2cf05-eks-projects-f96cd589.vercel.app",
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 const anthropic = new Anthropic();
 
